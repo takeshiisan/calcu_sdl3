7dce776efab4a30c970b2959aa0a146933b54a26
takeshiisan <takeshisan29@gmail.com>
2025-05-16 08:47:16 +0800

    idk

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,12 @@
 #include "SDL3/SDL.h"
 #include "SDL3_image/SDL_image.h"
 
-
-int main(int, char**){
+int main(){
     std::cout << "Hello, from calcu_sdl3!\n";
+
+    SDL_Window* window;
+    SDL_Renderer* renderer; 
+
+    SDL_CreateWindowAndRenderer("Calculator",640,480,SDL_WINDOW_OPENGL,&window, &renderer);
+
 }
